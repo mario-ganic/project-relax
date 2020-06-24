9c5d5a4ced2e415eaaedee40c97b18e5c995ba88
mario-ganic <62038359+mario-ganic@users.noreply.github.com>
2020-06-24 12:34:55 +0200

    few changes to everything

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+const text = document.querySelector('#text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+
+breathAnimation();
+
+
+function breathAnimation () {
+    text.innerHTML = 'Breathe In';
+    container.className = 'container grow';
+
+    setTimeout(() => {
+        text.innerText = 'Hold in';
+
+        setTimeout(() => {
+            text.innerText = 'Breathe Out'
+            container.className = 'container shrink';
+        }, holdTime);
+
+    }, breatheTime)
+}
+
+
+setInterval(breathAnimation, totalTime);
